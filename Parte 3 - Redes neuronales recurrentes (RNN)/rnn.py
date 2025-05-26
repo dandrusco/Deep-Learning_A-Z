d4d46fd5a27c6114fd8c948574148d4eacd9b5fc
dAndrusco <dandrusco@gmail.com>
2025-05-26 17:42:02 -0400

    Construir una RNR - Paso 5

--- Parte 3 - Redes neuronales recurrentes (RNN)/rnn.py
+++ Parte 3 - Redes neuronales recurrentes (RNN)/rnn.py
@@ -1,37 +1,54 @@
-# Recurrent Neural Network
+# Redes Neuronales Recurrentes (RNN)
 
 
 
-# Part 1 - Data Preprocessing
+# Parte 1 - Preprocedado de los datos
 
-# Importing the libraries
+# Importar las librerias
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-# Importing the training set
+# Importamos el dataset de entrenamiento con los datos de las acciones de google entre el 2012 y el 2016
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+# Nos quedaremos solo con la columnas de apertura de la bolsa del dataset (Open)
 training_set = dataset_train.iloc[:, 1:2].values
 
-# Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
+# Escalado de los datos, utilizaremos el escalado MinMaxScaler de procesado de  sklearn
+# Donde el valor mas pequeño se transforma en cero y el maximo en 1 
 sc = MinMaxScaler(feature_range = (0, 1))
+# Transformaremos nuestro conjunto de entrenamiento en un conjunto de entrenamiento escalado
 training_set_scaled = sc.fit_transform(training_set)
 
-# Creating a data structure with 60 timesteps and 1 output
+# Crearemos una estructura de datos con 60 timesteps y 1 salida, lo que equivale que la RNN mirara 60 dias hacia atras
+# 60 dias equivale a 1 trimestre, teniendo ese dato tomar el dato 0 al 60 y tratara de predecir el dia 61 (dia siguiente)
+# Luego tomara del dia 1 al 61 para predecir el dia 62 y asi sucesivamente 
+# Crearemos 2 listas, una para el conjunto de entrenamiento (la entrada) y la otra para el conjunto de salida
 X_train = []
 y_train = []
+
+# Comenzaremos a rellenar los datos con un bucle for, la primera iteracion partira en el 60 y el ultimo sera 1258
 for i in range(60, 1258):
+    # El X_train se debera llevar bloques de 60, por lo tanto nos vamos 60 dias hacia atras i-60 y llegara hasta el 60
+    # Como los conjuntos de datos son bidimencionales debemos parasarle un 0 
     X_train.append(training_set_scaled[i-60:i, 0])
+    # En y_train apendizaremos el dia de la prediccion actual 61, 62, 63... (tambien le debemos pasar un 0 al final)
     y_train.append(training_set_scaled[i, 0])
+# Vamos a pescar ambos conjuntos para transformarlas en matrices de numpy
 X_train, y_train = np.array(X_train), np.array(y_train)
 
-# Reshaping
+# Redimension de los datos
+# Con reshape podemos agregar una nueva dimencion al conjunto de datos
+# El primer argumento es lo que queremos redimencionar, el segundo parametro es una tupla
+# En la tupla le pasamos la nueva estructura, la primera es [0] para obtener el numero de filas
+# la segunda es [1] para el numero de columnas y 1 como variable predictoria 
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+# Al ejecurar ahora el X_train se transformo a 1198, 60, 1
+# Al abrir el X_train en Variable Explirer vemos una solo columna, abajo en Axis podemos cambiar a las otras 0, 1 o 2
 
 
-
-# Part 2 - Building the RNN
+# Parte 2 - Construir la RNN
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
@@ -69,7 +86,7 @@ regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
 
 
-# Part 3 - Making the predictions and visualising the results
+# Parte 3 - Ajustar las predicciones y visualizar los resultados
 
 # Getting the real stock price of 2017
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
