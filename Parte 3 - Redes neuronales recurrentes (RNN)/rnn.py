8cec85c8453d72286b7f0f3cfa83a0c4caaefb67
dAndrusco <dandrusco@gmail.com>
2025-05-27 15:52:40 -0400

    Construir una RNR - Paso 12

--- Parte 3 - Redes neuronales recurrentes (RNN)/rnn.py
+++ Parte 3 - Redes neuronales recurrentes (RNN)/rnn.py
@@ -1,7 +1,6 @@
 # Redes Neuronales Recurrentes (RNN)
 
 
-
 # Parte 1 - Preprocedado de los datos
 
 # Importar las librerias
@@ -50,49 +49,64 @@ X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
 # Parte 2 - Construir la RNN
 
-# Importing the Keras libraries and packages
+# Importamos las librerias de Keras
 from keras.models import Sequential
+# De las capas de Keras importaremos la densa
 from keras.layers import Dense
+# De las capas de Keras importaremos la LSTM para apilar 
 from keras.layers import LSTM
+# De las capas de Keras importaremos la Dropout para prevenir el overfiting
 from keras.layers import Dropout
 
-# Initialising the RNN
+# Crearemos una regrecion de secuencias de capas
 regressor = Sequential()
 
-# Adding the first LSTM layer and some Dropout regularisation
+# Añadiremos la primera capara de LSTM y la regulariacion por Dropout
+# A la regrecion le añadiremos una capa de LSTM, como paramatro le pasaremos cuantas neuronas queremos meter (units)
+# EL segundo parametro es la secuencia de retorno, si queremos unas varias capas apiladas, debemos ponerlo en True
+# El input_shape corresponde al tamaño y dimenciones de los datos 
 regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+# Ahora le añadiremos un Dropout para desactivar algunas neurnas del paso anterior para prevenir el overffiting 20%
 regressor.add(Dropout(0.2))
 
-# Adding a second LSTM layer and some Dropout regularisation
+# Añadiremos la segunda capa de LSTM y la regulariacion por Dropout
+# Sera la misma capa de LSTM y Dropout pero ya no necesitamos los datos de entrada
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 
-# Adding a third LSTM layer and some Dropout regularisation
+# Añadiremos la tercera capa de LSTM y la regulariacion por Dropout
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
+# Añadiremos la cuarta capa de LSTM y la regulariacion por Dropout
+# En la ultima capa ya no necesitamos return_sequences por que esta sera una capa densa
 regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
 
-# Adding the output layer
+# Añadiremos la capa de salida a la regresion, sera una capa densa con 1 neurona de salida
 regressor.add(Dense(units = 1))
 
-# Compiling the RNN
+# Compilamos la RNN, el optimizador sera adam que se utiliza para las redes neuronales recurrentes
+# El segundo parametro es de las perdidas a optimizar, utilizaremos mean_squared_error 
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
-# Fitting the RNN to the Training set
+# Ajustaremos la RNN a nuestro conjunto de entrenamiento
+# con fit ajustamos los datos, como parametro le pasamosla capa de entrada, prediccion de salida, epochs y batch_size
+# le pasaremos 100 iteraciones (epocas) y con el tamaño del bloque (batch_size) seran de 32 
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
-
+# Al compilar esta linea, la primera iteracion nos da un error (loss) de un 4% 
+#  38/38 [==============================] - 6s 50ms/step - loss: 0.0494
+# A medida que avanza y nos acercamos al 100, vemos como el error va disminuyendo, hasta un 0.15%
 
 
 # Parte 3 - Ajustar las predicciones y visualizar los resultados
 
-# Getting the real stock price of 2017
+# Obter el valor real de las acciones de Enero de 2017, para ello cargaremos el dataset Google_Stock_Price_Test
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+# Nos quedaremos solo con la columnas de apertura de la bolsa del dataset (Open)
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
-# Getting the predicted stock price of 2017
+# Predecir las acciones de Enero de 2017 con la RNN
 dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
 inputs = inputs.reshape(-1,1)
